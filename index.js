77d8aa254fe32eeafad15e1ecfb65f6f8cc5adfe
quallsma <quallsma@msu.edu>
2017-07-21 09:23:22 -0400

    Last Test, working project

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-export class RomanConverter{
+export default class RomanConverter{
 
     constructor(){
         this.roman_numerals = {
+            "M": 1000,
+            "CM": 900,
+            "D": 500,
+            "CD": 400,
+            "C": 100,
+            "XC": 90,
+            "L": 50,
+            "XL": 40,
+            "X": 10,
+            "IX": 9,
             "V": 5,
             "IV": 4,
             "I": 1
@@ -18,5 +28,21 @@ export class RomanConverter{
             }
         }
         return roman;
+    }
+
+    convertRomanToArabic(roman){
+        let arabic = 0;
+        let previous = 0;
+        let current = 0;
+        for(let x = 0; x < roman.length; x++) {
+            current = this.roman_numerals[roman.charAt(x)];
+            arabic += current;
+
+            if(current > previous && previous > 0)
+                arabic -= (2 * previous);
+
+            previous = current;
+        }
+        return arabic;
     }
 }
--- test/test.js
+++ test/test.js
@@ -1,11 +1,27 @@
 import { expect } from 'chai';
-import { RomanConverter } from '../index'
+import RomanConverter from '../index'
 
 describe("Mocha and Chai", () => {
     it("should be working", () => {
         expect(true).to.be.true;
     });
 });
+
+/* 
+        romanNumerals.put(1000, "M");
+        romanNumerals.put(900, "CM");
+        romanNumerals.put(500, "D");
+        romanNumerals.put(400, "CD");
+        romanNumerals.put(100, "C");
+        romanNumerals.put(90, "XC");
+        romanNumerals.put(50, "L");
+        romanNumerals.put(40, "XL");
+        romanNumerals.put(10 , "X");
+        romanNumerals.put(9, "IX");
+        romanNumerals.put(5, "V");
+        romanNumerals.put(4, "IV");
+        romanNumerals.put(1, "I");
+*/
 
 let converter = new RomanConverter();
 describe("Roman Numeral Converter ", () => {
@@ -23,7 +39,33 @@ describe("Roman Numeral Converter ", () => {
         });
 
         it("Should return IV when given 4", () => {
+            expect(converter.convertArabicToRoman(1989)).to.equals('MCMLXXXIX');
+        });
+
+        it("Should return MCMLXXXIX when given 1989", () => {
             expect(converter.convertArabicToRoman(4)).to.equals('IV');
+        });
+    });
+
+    describe("Convert to Arabic Number", () => {
+        it("Should return 1 when given I", () => {
+            expect(converter.convertRomanToArabic('I')).to.equals(1);
+        });
+
+        it("Should return 5 when given V", () => {
+            expect(converter.convertRomanToArabic('V')).to.equals(5);
+        });
+
+        it("Should return 4 when given IV", () => {
+            expect(converter.convertRomanToArabic('IV')).to.equals(4);
+        });
+
+        it("Should return 3 when given III", () => {
+            expect(converter.convertRomanToArabic('III')).to.equals(3);
+        });
+
+        it("Should return 1989 when given MCMLXXXIX", () => {
+            expect(converter.convertRomanToArabic('MCMLXXXIX')).to.equals(1989);
         });
     });
 });
